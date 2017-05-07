9ac2c50f4ca6b3bad7606896bce78662f6b4ad6b
Yoki <camillemagic@gmail.com>
2017-05-07 14:50:57 -0400

    Update organizer.ml

--- compiler/organizer.ml
+++ compiler/organizer.ml
@@ -1,52 +1,52 @@
 module A = Ast
-module C = Cast
+module S = Sast
 
 module StringMap = Map.Make(String)
 let node_num = ref 0
 
 let convert_binop = function
-    A.Add -> C.Add
-  | A.Sub -> C.Sub
-  | A.Mult -> C.Mult
-  | A.Div -> C.Div
-  | A.Mod -> C.Mod
-  | A.Equal -> C.Equal
-  | A.Neq -> C.Neq
-  | A.Less -> C.Less
-  | A.Leq -> C.Leq
-  | A.Greater -> C.Greater
-  | A.Geq -> C.Geq
-  | A.And -> C.And
-  | A.Or -> C.Or
+    A.Add -> S.Add
+  | A.Sub -> S.Sub
+  | A.Mult -> S.Mult
+  | A.Div -> S.Div
+  | A.Mod -> S.Mod
+  | A.Equal -> S.Equal
+  | A.Neq -> S.Neq
+  | A.Less -> S.Less
+  | A.Leq -> S.Leq
+  | A.Greater -> S.Greater
+  | A.Geq -> S.Geq
+  | A.And -> S.And
+  | A.Or -> S.Or
 
 let convert_unop = function
-  A.Neg -> C.Neg
-| A.Not -> C.Not
+  A.Neg -> S.Neg
+| A.Not -> S.Not
 
 let convert_num = function
-    A.Num_Int(a) -> C.Num_Int(a)
-  | A.Num_Float(a) -> C.Num_Float(a)
+    A.Num_Int(a) -> S.Num_Int(a)
+  | A.Num_Float(a) -> S.Num_Float(a)
 
 let convert_var_type = function
-    A.Int_t -> C.Int_t
-  | A.Float_t -> C.Float_t
-  | A.String_t -> C.String_t
-  | A.Bool_t -> C.Bool_t
-  | A.Node_t -> C.Node_t
-  | A.Graph_t -> C.Graph_t
-  | A.List_Int_t -> C.List_Int_t
-  | A.List_Float_t -> C.List_Float_t
-  | A.List_String_t -> C.List_String_t
-  | A.List_Node_t -> C.List_Node_t
-  | A.List_Graph_t -> C.List_Graph_t
-  | A.List_Bool_t -> C.List_Bool_t
-  | A.Dict_Int_t -> C.Dict_Int_t
-  | A.Dict_Float_t -> C.Dict_Float_t
-  | A.Dict_String_t -> C.Dict_String_t
-  | A.Dict_Node_t -> C.Dict_Node_t
-  | A.Dict_Graph_t -> C.Dict_Graph_t
-  | A.Void_t -> C.Void_t
-  | A.Null_t -> C.Null_t
+    A.Int_t -> S.Int_t
+  | A.Float_t -> S.Float_t
+  | A.String_t -> S.String_t
+  | A.Bool_t -> S.Bool_t
+  | A.Node_t -> S.Node_t
+  | A.Graph_t -> S.Graph_t
+  | A.List_Int_t -> S.List_Int_t
+  | A.List_Float_t -> S.List_Float_t
+  | A.List_String_t -> S.List_String_t
+  | A.List_Node_t -> S.List_Node_t
+  | A.List_Graph_t -> S.List_Graph_t
+  | A.List_Bool_t -> S.List_Bool_t
+  | A.Dict_Int_t -> S.Dict_Int_t
+  | A.Dict_Float_t -> S.Dict_Float_t
+  | A.Dict_String_t -> S.Dict_String_t
+  | A.Dict_Node_t -> S.Dict_Node_t
+  | A.Dict_Graph_t -> S.Dict_Graph_t
+  | A.Void_t -> S.Void_t
+  | A.Null_t -> S.Null_t
 
 
 let rec get_entire_name m aux cur_name =
@@ -60,22 +60,22 @@ let increase_node_num =
   !(node_num) - 1
 
 let rec convert_expr m = function
-    A.Num_Lit(a) -> C.Num_Lit(convert_num a)
-|   A.Null -> C.Null
-|   A.String_Lit(a) -> C.String_Lit(a)
-|   A.Bool_lit(a) -> C.Bool_lit(a)
-|   A.Node(a) -> node_num := (!node_num + 1); C.Node(!node_num - 1, convert_expr m a)
-|   A.EdgeAt(a,b,c) -> C.EdgeAt(convert_expr m a, convert_expr m b, convert_expr m c) 
-|   A.Binop(a,b,c) -> C.Binop(convert_expr m a, convert_binop b, convert_expr m c)
-|   A.Unop(a,b) -> C.Unop(convert_unop a, convert_expr m b)
-|   A.Id(a) -> C.Id(a)
-|   A.Assign(a,b) -> C.Assign(a, convert_expr m b)
-|   A.Noexpr -> C.Noexpr
-|   A.ListP(a) -> C.ListP(convert_expr_list m a)
-|   A.DictP(a) -> C.DictP(convert_dict_list m a)
-|   A.Call(a,b) -> C.Call(get_entire_name m a a, convert_expr_list m b)
-|   A.CallDefault(a,b,c) -> C.CallDefault(convert_expr m a, b, convert_expr_list m c)
-|   A.Ganalysis(a) -> C.Ganalysis(convert_e_list m a)
+    A.Num_Lit(a) -> S.Num_Lit(convert_num a)
+|   A.Null -> S.Null
+|   A.String_Lit(a) -> S.String_Lit(a)
+|   A.Bool_lit(a) -> S.Bool_lit(a)
+|   A.Node(a) -> node_num := (!node_num + 1); S.Node(!node_num - 1, convert_expr m a)
+|   A.EdgeAt(a,b,c) -> S.EdgeAt(convert_expr m a, convert_expr m b, convert_expr m c) 
+|   A.Binop(a,b,c) -> S.Binop(convert_expr m a, convert_binop b, convert_expr m c)
+|   A.Unop(a,b) -> S.Unop(convert_unop a, convert_expr m b)
+|   A.Id(a) -> S.Id(a)
+|   A.Assign(a,b) -> S.Assign(a, convert_expr m b)
+|   A.Noexpr -> S.Noexpr
+|   A.ListP(a) -> S.ListP(convert_expr_list m a)
+|   A.DictP(a) -> S.DictP(convert_dict_list m a)
+|   A.Call(a,b) -> S.Call(get_entire_name m a a, convert_expr_list m b)
+|   A.CallDefault(a,b,c) -> S.CallDefault(convert_expr m a, b, convert_expr_list m c)
+|   A.Ganalysis(a) -> S.Ganalysis(convert_e_list m a)
 
 
 and convert_expr_list m = function
@@ -101,10 +101,10 @@ and convert_e_list m = function
     
 
 let convert_edge_graph_list m = function
-  {A.graphs = g; A.edges = e} -> {C.graphs = convert_expr_list m g; C.edges = convert_expr_list m e}
+  {A.graphs = g; A.edges = e} -> {S.graphs = convert_expr_list m g; C.edges = convert_expr_list m e}
 
 let convert_formal = function
-  | A.Formal(v, s) -> C.Formal(convert_var_type v, s)
+  | A.Formal(v, s) -> S.Formal(convert_var_type v, s)
 
 let convert_formal_list = function
     [] -> []
@@ -156,35 +156,35 @@ let rec bfser m result = function
 
 (* convert stament in A to C, except those Var_dec and Func, we will convert them separately *)
 let rec convert_stmt m = function
-    A.Expr(a) -> C.Expr(convert_expr m a)
-  | A.Return(a) -> C.Return(convert_expr m a)
-  | A.For(e1, e2, e3, stls) -> C.For(convert_expr m e1, convert_expr m e2, convert_expr m e3, List.map (convert_stmt m) stls)
-  | A.If(e, stls1, stls2) -> C.If(convert_expr m e, List.map (convert_stmt m) stls1, List.map (convert_stmt m) stls2)
-  | A.While(e, stls) -> C.While(convert_expr m e, List.map (convert_stmt m) stls)
-  | _ -> C.Expr(C.Noexpr)
+    A.Expr(a) -> S.Expr(convert_expr m a)
+  | A.Return(a) -> S.Return(convert_expr m a)
+  | A.For(e1, e2, e3, stls) -> S.For(convert_expr m e1, convert_expr m e2, convert_expr m e3, List.map (convert_stmt m) stls)
+  | A.If(e, stls1, stls2) -> S.If(convert_expr m e, List.map (convert_stmt m) stls1, List.map (convert_stmt m) stls2)
+  | A.While(e, stls) -> S.While(convert_expr m e, List.map (convert_stmt m) stls)
+  | _ -> S.Expr(S.Noexpr)
 
 
 let rec get_body_from_body_c m = function
     [] -> []
-  | A.Var_dec(A.Local(_, name, v))::tl when v <> A.Noexpr -> C.Expr(C.Assign(name, convert_expr m v)) :: (get_body_from_body_c m tl)
+  | A.Var_dec(A.Local(_, name, v))::tl when v <> A.Noexpr -> S.Expr(S.Assign(name, convert_expr m v)) :: (get_body_from_body_c m tl)
   | A.Var_dec(A.Local(_, _, v))::tl when v = A.Noexpr -> (get_body_from_body_c m tl)
   | _ as x::tl -> (convert_stmt m x) :: (get_body_from_body_c m tl)
 
 let rec get_local_from_body_c = function
     [] -> []
-  | A.Var_dec(A.Local(typ, name, _))::tl -> C.Formal(convert_var_type typ, name) :: (get_local_from_body_c tl)
+  | A.Var_dec(A.Local(typ, name, _))::tl -> S.Formal(convert_var_type typ, name) :: (get_local_from_body_c tl)
   | _::tl -> get_local_from_body_c tl
 
 (* convert the horizental level function list in A to C *)
 let rec convert_func_list_c m = function
     [] -> []
   | A.Func{A.returnType = r; A.name = n; A.args = a; A.body = b} :: tl -> {
-    C.returnType = convert_var_type r;
-    C.name = get_entire_name m n n;
-    C.args = convert_formal_list a;
-    C.body = get_body_from_body_c m b;
-    C.locals = get_local_from_body_c b;
-    C.pname = if n = "main" then "main" else get_entire_name m (StringMap.find n m) (StringMap.find n m)
+    S.returnType = convert_var_type r;
+    S.name = get_entire_name m n n;
+    S.args = convert_formal_list a;
+    S.body = get_body_from_body_c m b;
+    S.locals = get_local_from_body_c b;
+    S.pname = if n = "main" then "main" else get_entire_name m (StringMap.find n m) (StringMap.find n m)
   } :: (convert_func_list_c m tl)
   | _::tl -> convert_func_list_c m tl
 
