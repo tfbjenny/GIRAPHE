b3b4c13e1fd562a7ac6b101127dbe27915c867b8
Yoki <camillemagic@gmail.com>
2017-05-07 13:40:36 -0400

    Update ast.ml

--- compiler/ast.ml
+++ compiler/ast.ml
@@ -58,7 +58,6 @@ type expr =
 |   String_Lit of string
 |   Bool_lit of bool
 |   Node of expr
-| 	Graph_Link of expr * graph_op * expr * expr
 |   EdgeAt of expr * expr * expr
 | 	Binop of expr * binop * expr
 |  	Unop of unop * expr
