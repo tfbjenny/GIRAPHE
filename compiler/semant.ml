fbfb18b0918f2ab9b1c0dcdabea1249f16c42701
wutongy <tw2568@columbia.edu>
2017-05-07 23:04:24 -0400

    sam

--- compiler/semant.ml
+++ compiler/semant.ml
@@ -593,7 +593,7 @@ let check_function func_map func =
                     (match n with
                         | "setVisited" -> Node_t
                         | "isVisted" -> Node_t
-                    ) n
+                    )
                   | _ -> unsupport_operation_error (string_of_typ typ) n
     in
     (* check statement *)
