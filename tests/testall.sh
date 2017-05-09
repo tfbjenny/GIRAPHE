e35c4cc20789ee68b233a5d64927f4ade5aec142
tfbjenny <tfbjenny@outlook.com>
2017-05-09 18:50:55 -0400

    sement1

--- tests/testall.sh
+++ tests/testall.sh
@@ -165,12 +165,6 @@ LLIFail() {
 
 which "$LLI" >> $globallog || LLIFail
 
-if [ ! -f printbig.o ]
-then
-    echo "Could not find printbig.o"
-    echo "Try \"make printbig.o\""
-    exit 1
-fi
 
 if [ $# -ge 1 ]
 then
