f376e527da8613617c6d5c19306b96bb5c1f8306
wutongy <tw2568@columbia.edu>
2017-05-09 15:05:48 -0400

    dijk

--- compiler/lib/utils.c
+++ compiler/lib/utils.c
@@ -94,8 +94,10 @@ void heapify(struct minHeap* hp, int size){
 	
 void insertData(struct minHeap* hp, struct Edge* data){
     if(getListSize(hp->array) > 0){
-	  printf("size > 0\n");
       addList(hp->array, data);
+	  printf("---------------\n");
+	  printList(hp->array);
+	  printf("---------------\n");
       int size = getListSize(hp->array);
       heapify(hp, size);
     } else {
