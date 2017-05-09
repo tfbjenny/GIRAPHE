ca919f0c371b1a667e2ff2d605c6a2bf5bb3e1e4
wutongy <tw2568@columbia.edu>
2017-05-09 15:44:35 -0400

    dijk

--- compiler/lib/utils.c
+++ compiler/lib/utils.c
@@ -1058,8 +1058,8 @@ int32_t dijkstra(struct Graph* g, struct Node* sour, struct Node* dest) {
 			hashmap_put(prev, v, NULL);
 		}
 		printf("%d", (*(int*)hashmap_get(dist, v)));
-		printList(minH->array);
 		int tmp = (*(int*)hashmap_get(dist, v));
+		printList(minH->array);
 		struct Edge eg = createEdge(sour, v, RIGHT_LINK, tmp, 0.0, 0, NULL);
 		printList(minH->array);
 		insertData(minH, &eg);
