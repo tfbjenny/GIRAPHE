2e93c00d1f3f5ce7c9b415f6fb252331521f6747
Yoki <camillemagic@gmail.com>
2017-05-09 19:36:44 -0400

    Update semantic_check.ml

--- tests/semantic_check/semantic_check.ml
+++ tests/semantic_check/semantic_check.ml
@@ -4,9 +4,9 @@ open Printf
 let _ =
     let lexbuf = Lexing.from_channel stdin in
     let ast = Parser.program Scanner.token lexbuf in
-    let cast = Organizer.convert ast in
+    let sast = Checker.convert ast in
     try 
-        Semant.check cast
+        Semant.check sast
     with 
         Semant.SemanticError(m) -> print_endline m
         (* Semant.SemanticError(m) -> raise (Failure(m)) *)
