fe6c7154eed78a33963c4e541ab3e6ddfe6aac80
tfbjenny <tfbjenny@users.noreply.github.com>
2017-05-09 23:27:21 -0400

    Update utils.h

--- compiler/lib/utils.h
+++ compiler/lib/utils.h
@@ -63,6 +63,23 @@ struct Queue {
 	struct List* lst;
 };
 
+struct minHeap {
+    //int size;
+    int32_t type;
+    struct List* array;
+};
+
+/************************************
+	Heap Methods
+************************************/
+struct minHeap* initList(int32_t type);
+void swap(struct List* list, int index1, int index2);
+int eCompare(struct minHeap* hp, struct Edge* lchild, struct Edge* rchild);
+void heapify(struct minHeap* hp, int size);
+void insertData(struct minHeap* hp, struct Edge* data);
+struct Edge* getMinValue(struct minHeap* hp);
+int32_t printHeap(struct minHeap* hp);
+void decreasePriority(struct minHeap* hp, struct Edge* e);
 /************************************
 	Queue Methods
 ************************************/
@@ -131,7 +148,8 @@ int32_t graphNumOfEdges(struct Graph* g);
 struct List* graphGetChildNodes(struct Graph* g, struct Node* rt);
 int32_t printGraph(struct Graph* g);
 bool setAllUnvisited(struct Graph* g);
-bool dfs(struct Graph* g, struct Node* n);
+struct List* dfs(struct Graph* g, struct Node* n);
 bool bfs(struct Graph* g, struct Node* n);
 bool containsNode(struct Graph* g, struct Node* n);
+int32_t dijkstra(struct Graph* g, struct Node* sour, struct Node* dest);
 #endif /* #ifndef _UTILS_H_ */
