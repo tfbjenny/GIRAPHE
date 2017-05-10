adeb9f3befc728fdf71c829c2c6e21079a51f17b
Yoki <camillemagic@gmail.com>
2017-05-10 01:34:32 -0400

    Update pig.sh

--- compiler/pig.sh
+++ compiler/pig.sh
@@ -1,6 +1,6 @@
 make clean
 ocamlc -c ast.ml
-ocamlc -c cast.ml
+ocamlc -c sast.ml
 ocamlyacc parser.mly
 ocamllex scanner.mll
 ocamlc -c parser.mli # compile parser types
@@ -8,5 +8,5 @@ ocamlc -c scanner.ml
 ocamlc -c parser.ml
 ocamlc -c organizer.ml
 ocamlc -c parserize_cast.ml
-ocamlc -o pig ast.cmo cast.cmo parser.cmo scanner.cmo organizer.cmo parserize_cast.cmo
+ocamlc -o pig ast.cmo sast.cmo parser.cmo scanner.cmo checker.cmo parserize_sast.cmo
 #./pig
