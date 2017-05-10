6c39637cd5d9587ea1f070924b47df087efb0c72
Yoki <camillemagic@gmail.com>
2017-05-10 01:34:53 -0400

    Update pig.sh

--- compiler/pig.sh
+++ compiler/pig.sh
@@ -6,7 +6,7 @@ ocamllex scanner.mll
 ocamlc -c parser.mli # compile parser types
 ocamlc -c scanner.ml
 ocamlc -c parser.ml
-ocamlc -c organizer.ml
-ocamlc -c parserize_cast.ml
+ocamlc -c checker.ml
+ocamlc -c parserize_sast.ml
 ocamlc -o pig ast.cmo sast.cmo parser.cmo scanner.cmo checker.cmo parserize_sast.cmo
 #./pig
